d3293fa3fa8c600bb3f0d2d6f48ff044e476e795
Yang Du <90231054+yang-example@users.noreply.github.com>
2024-10-08 13:37:50 -0500

    Add files via upload

---
+++ hello/code.js
@@ -0,0 +1,6 @@
+
+document.getElementById("b1").addEventListener("click", onclick)
+
+function onclick () { 
+    document.getElementById("heading").innerHTML = document.getElementById("data").value
+}
